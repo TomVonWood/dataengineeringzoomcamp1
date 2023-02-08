ff97979b274f2e436c5928d1f0d671b31241e339
TomVonWood <115629329+TomVonWood@users.noreply.github.com>
2023-02-08 22:48:41 +0100

    bigquery week 3

---
+++ week3_2/question 1.sql
@@ -0,0 +1,3 @@
+    SELECT 
+    count(dispatching_base_num) dispatching_base_num,
+    FROM `de-bootcamp123.fhv.fhv_2019` 
---
+++ week3_2/questoin3.sql
@@ -0,0 +1,5 @@
+SELECT 
+count(*),
+
+ FROM `de-bootcamp123.fhv.fhv_native` 
+ where PUlocationID is null and DOlocationID is null
